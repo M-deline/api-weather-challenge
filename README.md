3ee006698f74a2eaafb7d937ecec6177cc2cfc09
Madeline K <madkovanda@gmail.com>
2023-09-16 15:02:37 -0600

    update

--- README.md
+++ README.md
@@ -1,6 +1,16 @@
 # api-weather-challenge
 ## Overview
+- This challenge is all about learning how to fetch an API to use the information from a different website and be able to take that data and have it display on your own website. This challenge was super fun because it included a lot of differet parts that we had learned about in class. There are a lot of ways to get an API to work so this challenge was fun because I was able to research different techniques to call the API. 
+- The goal of this challenge is to take the information from openweathermap.org and have it display when the user submits the name of their chosen city. This is so that the user is able to see the current weather for the current day but also for 5 days out so that if they have plans or a vacation they are able to plan accordingly. 
+
+## Screenshot and Deployed website
+- Here is the link to the deployed website. 
 
 ## Takeaways
+- This challenge was a success since I learned a lot about API's. One thing in particular is that there is a lot of information online This is because there are different ways to format the JavaScript and there are different libraries as well (JQuery vs no JQuery). This was confusing at times since I would accidentally mix the two and then get an error. 
+- I had fun with the styling on bootstrap since while we did learn about it in class and spend a bit on Bootsrap, I was excited to take this opportunity to use it again and practice. 
+- A struggle I also had was making so many functions. It was confusing to make sure that I was using each API doc from the website correctly. Luckily, I remembered how important console . log is to make sure each thing is at least working before implementing the rest of the code. 
 
 ## Contributions
+- I would like to give a huge thank you and shout out to Corey Yates. She is a tutor from BCS and she helped me a ton during ths challenge. She helped me format my JavaScript correctly to make sure that the API was correctly written. She helped me with using json response as well and making sure that the lat and lon parameters and data was correct in order for the website to display properly. 
+- I would also like to give thanks to another BCS student support tutor, Yanga who helped me to get the data from the API to show up in the HTML. I was stuck on how to get the data from the console log from .main into my JavaScript and HTML and Yanga walked me through the steps on how to get there. 
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 //https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
 const searchButton = document.querySelector('#search-button');
-var apiKey = "9f87752a06c22d77f56671cc917855d3";
+var apiKey = "f8332907dad2ea3f708292660e2f5f7b";
 var history = []
 //prevent default refresh
 var documentForm = function (event) {
@@ -20,70 +20,88 @@ function getGeo(name) {
     var lon = data[0].lon
     getCurrentWeather(lat, lon)
   })
-  .catch(err => console.log(err))
+    .catch(err => console.log(err))
 }
 function getCurrentWeather(lat, lon) {
   fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`)
-  .then(response => {
-    return response.json()
-  }).then(data => {
-    console.log(data)
-   let timeStamp = dayjs.unix(data.dt)
-   let dataDisplay = timeStamp.format("MMMM DD, YYYY")
-   document.getElementById("city-name").textContent = dataDisplay
-   document.getElementById("date").lastChild.textContent = time
-   var urlIcon = 
-   "https://openweathermap.org/img/wn/10d@2x.png"
-   var temp = data.main.temp;
-   console.log(temp)
-   var humidity = data.main.humidity;
-   console.log(humidity)
-   var wind = data.wind.speed;
-   console.log(wind)
-  var temp = document.getElementById("temp")
-  var humidity = document.getElementById("humidity")
-  var wind = document.getElementById("wind")
-  var icon = document.getElementById("icon")
-  
-   document.getElementById("temp").textContent = temp;
-   document.getElementById("humidity").textContent = humidity;
-   document.getElementById("wind").textContent = wind;
-   document.getElementById("icon").textContent = urlIcon;
-    getForecast(lat, lon)
-  }) 
-  .catch(err => console.log(err))
-  
+    .then(response => {
+      return response.json()
+    }).then(data => {
+      console.log(data)
+      let timeStamp = dayjs.unix(data.dt)
+      let dataDisplay = timeStamp.format("MMMM DD, YYYY")
+      document.getElementById("city-name").textContent = dataDisplay
+      document.getElementById("date").lastChild.textContent = time
+
+      var weatherDiv = document.createElement("div")
+      var tempElement = document.createElement("p");
+      var humidityElement = document.createElement("p");
+      var windElement = document.createElement("p");
+      var iconElement = document.createElement("p");
+      
+      tempElement.textContent = "temp" + tempCurrent + f // Assuming 'temp' is in Celsius
+      humidityElement.textContent = "humidity:" + response.main.humidity;
+      windElement.textContent = "wind" + response.wind.speed;
+
+      weatherDiv.appendChild(tempElement);
+      weatherDiv.appendChild(humidityElement);
+      weatherDiv.appendChild(windElement);
+
+
+      getForecast(lat, lon)
+    })
+    .catch(err => console.log(err))
+
 }
 
 function getForecast(lat, lon) {
   fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`)
-  .then(response => {
-    return response.json()
-  }).then(data => {
-    console.log(data)
-  }) 
-  .catch(err => console.log(err))
-  
-}
-// var displayCurrentData = function(city, data) {
-///  var divCurrent = document.createElement("div")
-// var tempEl = document.createElement("p");
-// var humidityEl = document.createElement("p");
-// var windSpeedEl = document.createElement("p");
-  //somehting main
-  //Endpoints to dislay current data 
-  // var tempCurrent = Math.round(data.current.temp);
-  // var humidity = Math.round(data.current.humidity);
-  // var windSpeed = data.current.wind_speed;
-  // var uvIndex = data.current.uvi;
-  // var iconCurrent = data.current.weather[0].icon;
+    .then(response => {
+      return response.json()
+    }).then(data => {
+      console.log(data)
+    })
+    .catch(err => console.log(err))
 
-function getCondition (param1, param2) {
-  fetch (`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`)
+}
+
+
+function getCondition(param1, param2) {
+  fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`)
 }
 
 //filter fr 5 day forecast
-searchButton.addEventListener('click', function() {
+searchButton.addEventListener('click', function () {
   var cityName = document.getElementById("search-form").value
   getGeo(cityName)
 })
+
+// In your JavaScript code, you should use textContent to update the content of these elements like the example below.
+// Example
+// // Inside getCurrentWeather function
+// var tempElement = document.getElementById("temp");
+// var humidityElement = document.getElementById("humidity");
+// var windElement = document.getElementById("wind");
+// var iconElement = document.getElementById("icon");
+
+// tempElement.textContent = `${temp} °C`; // Assuming 'temp' is in Celsius
+// humidityElement.textContent = `${humidity}%`;
+// windElement.textContent = `${wind} m/s`;
+// iconElement.setAttribute('src', urlIcon); // Set the 'src' attribute for the image
+// Ensure that you have the urlIcon variable set correctly. You currently have a hardcoded URL for the weather icon. You should replace it with the actual URL you receive from the API response. The example below is an example, so make sure you check the return data to get the right link.
+// Example
+// var urlIcon = `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`;
+// You have a function getCondition(param1, param2) that seems to be incomplete. If you want to fetch weather conditions for a specific location, you should pass the lat and lon parameters to that function and make a fetch request inside it. The example below is how you can structure the getCondition function.
+// Remember to call getCondition with the appropriate lat and lon values when needed.
+// Example
+// function getCondition(lat, lon) {
+//   fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`)
+//     .then(response => {
+//       return response.json()
+//     })
+//     .then(data => {
+//       // Handle the data here
+//     })
+//     .catch(err => console.log(err))
+// }
+// Finally, make sure your getForecast function is complete and handles the forecast data properly. To ensure that the getForecast function is complete and handles the forecast data properly, you need to process the data returned from the API and update the DOM elements with the relevant forecast information.
