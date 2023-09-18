f04af7edcbb7e79ffc035ca72d76ae2354f2bb85
Madeline K <madkovanda@gmail.com>
2023-09-17 18:11:15 -0600

    update again

--- README.md
+++ README.md
@@ -7,11 +7,12 @@
 - Here is the link to the deployed website. 
 
 ## Takeaways
-- This challenge was a success since I learned a lot about API's. One thing in particular is that there is a lot of information online This is because there are different ways to format the JavaScript and there are different libraries as well (JQuery vs no JQuery). This was confusing at times since I would accidentally mix the two and then get an error. 
+- This challenge was a fun way to learn about APIs. One thing in particular is that there is a lot of information online This is because there are different ways to format the JavaScript and there are different libraries as well (JQuery vs no JQuery). This was confusing at times since I would accidentally mix the two and then get an error. 
 - I had fun with the styling on bootstrap since while we did learn about it in class and spend a bit on Bootsrap, I was excited to take this opportunity to use it again and practice. 
-- A struggle I also had was making so many functions. It was confusing to make sure that I was using each API doc from the website correctly. Luckily, I remembered how important console . log is to make sure each thing is at least working before implementing the rest of the code. 
+- A struggle I also had was the local storage. I couldn't get it to take the coordinates and make it just the city name. I also struggled with getting the 5 day forecast to work properly and kept getting a 400 bad request error.
 
 ## Contributions
+- “JavaScript and Localstorage in a Nutshell .” Blog by Tiny, 1 June 2023, www.tiny.cloud/blog/javascript-localstorage/. 
 - I would like to give a huge thank you and shout out to Corey Yates. She is a tutor from BCS and she helped me a ton during ths challenge. She helped me format my JavaScript correctly to make sure that the API was correctly written. She helped me with using json response as well and making sure that the lat and lon parameters and data was correct in order for the website to display properly. 
-- I would also like to give thanks to another BCS student support tutor, Yanga who helped me to get the data from the API to show up in the HTML. I was stuck on how to get the data from the console log from .main into my JavaScript and HTML and Yanga walked me through the steps on how to get there. 
-// thopstadredner a million thanks to them
+- I would also like to give thanks to another BCS student support tutor, thopstadredner, Drew, and Yanga who helped me to get the data from the API to show up in the HTML. I was stuck on how to get the data from the console log from .main into my JavaScript and HTML and Yanga walked me through the steps on how to get there. 
+
--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 //https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
 const searchButton = document.querySelector('#search-button');
-var apiKey = "f8332907dad2ea3f708292660e2f5f7b";
+var apiKey = "cdfc743396b214e68c6bc948dddc6bfc";
 var history = []
+
 //prevent default refresh
 var documentForm = function (event) {
   event.preventDefault();
 }
 
-// fetch() from that AI helper tool in BCS
+// var recentSearches = JSON.parse(localStorage.getItem("searches"));
+// var searchHistory = document.getElementById("history");
+
 function displayData(data) {
   const dataContainer = document.getElementById('data-container');
   dataContainer.textContent = JSON.stringify(data);
@@ -16,6 +19,8 @@ function getGeo(name) {
   fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${name}&limit=1&appid=${apiKey}`).then(response => {
     return response.json()
   }).then(data => {
+    localStorage.setItem("city", JSON.stringify(data));
+    console.log(data);
     var lat = data[0].lat
     var lon = data[0].lon
     getCurrentWeather(lat, lon)
@@ -28,6 +33,8 @@ function getCurrentWeather(lat, lon) {
       return response.json()
     }).then(data => {
       console.log(data)
+      localStorage.setItem("city", JSON.stringify(data));
+      console.log(data);
       let timeStamp = dayjs.unix(data.dt)
       let dataDisplay = timeStamp.format("MMMM DD, YYYY")
 
@@ -39,8 +46,7 @@ function getCurrentWeather(lat, lon) {
       getForecast(lat, lon)
     })
     .catch(err => console.log(err))
-
-}
+  }
 
 function getForecast(lat, lon) {
   fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`)
@@ -48,18 +54,58 @@ function getForecast(lat, lon) {
       return response.json()
     }).then(data => {
       console.log(data)
+      
     })
     .catch(err => console.log(err))
 
 }
-
-
 function getCondition(param1, param2) {
   fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`)
 }
 
-//filter fr 5 day forecast
+
+
+window.onload = function() {
+
+  // Retrieve the users name.
+  var test = localStorage.getItem('city')
+
+  if (test != "undefined" || test != "null") {
+    document.getElementById('history').innerHTML = "Recent " + test + "!";
+  } else
+    document.getElementById('history').innerHTML = "Recent!";
+  }
+
+
+
+//filter for 5 day forecast
 searchButton.addEventListener('click', function () {
   var cityName = document.getElementById("search-form").value
   getGeo(cityName)
 })
+
+
+// var searchHistory = document.getElementById("search-history");
+// var city = document.getElementById("city-name");
+// searchHistory.addEventListener("click", function (event) {
+//   event.preventDefault();
+//   var searchCity = city.value
+//   // Adds searched city to local storage
+//   submitSearch(searchCity);
+//   addRecentSearch(searchCity);
+// });
+// function addRecentSearch(city) {
+//   var recentButton = document.createElement("button");
+//   recentButton.textContent = city;
+//   recentButton.addEventListener("click", function () {
+//     submitSearch(city);
+//   });
+//   searchHistory.appendChild(recentButton);
+// }
+// function submitSearch(searchCity) {
+//   recentSearches.push(searchCity);
+//   console.log(recentSearches)
+//   localStorage.setItem("searches", JSON.stringify(recentSearches));
+//   getCurrentWeather(searchCity);
+// }
+
